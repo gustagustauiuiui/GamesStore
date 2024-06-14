f2a72f0f4e76630b86fdd71c424a8d6f2e99dd11
Gustavo Lage <lageg901@gmail.com>
2024-06-14 16:16:40 -0400

    feat(store/recomendacoes/carrossel): mudanca de estato do carrossel

--- resources/js/Components/Organismos/Carrossel.jsx
+++ resources/js/Components/Organismos/Carrossel.jsx
@@ -3,19 +3,40 @@ import React from "react"
 
 export default function Carrossel({ items = [] }) {
 
-    const [atual, setAtual] = React.useState(items[0]);
+    const atualIndex = 0;
+    const max = items.length-1;
+    const [atual, setAtual] = React.useState(items[atualIndex]);
+
+    const handleNext = (e) => {
+        e.preventDefault();
+        atualIndex = atualIndex - 1 < 0 ? 0 : atualIndex - 1;
+        setAtual(items[atualIndex]);
+    }
+
+    const handlePrev = () => {
+        e.preventDefault();
+        atualIndex = atualIndex + 1 > max ? max : atualIndex + 1;
+        setAtual(items[atualIndex]);
+    }
+
     console.log(atual);
 
     return (
         <div className="w-full h-full flex text-white justify-between">
-            <div className="w-[5%] h-full flex items-center justify-center">
+            <div
+                className="w-[5%] h-full flex items-center justify-center"
+                onClick={(e) => handlePrev(e)}
+            >
                 <button><ArrowLeft size={32} /></button>
             </div>
             <div>
                 {atual.nome}
 
             </div>
-            <div className="w-[5%] h-full  flex items-center justify-center">
+            <div
+                className="w-[5%] h-full  flex items-center justify-center"
+                onClick={(e) => handleNext(e)}
+            >
                 <button><ArrowRight size={32} /></button>
             </div>
         </div>
